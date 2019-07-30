90de2f96593241e5939ccd5b2103864a1002c719
Mihai Luca <mihailuca406@gmail.com>
2019-07-30 14:00:11 +0300

    improved sw (switch files) for big files. now creates directories instead of copying files

--- sw.sh
+++ sw.sh
@@ -3,7 +3,9 @@
 # Switch two files if they exist
 
 if [ -f "$1" ] && [ -f "$2" ]; then
-    cp "$1" temp
+    mkdir "${1}_${2}_sw_temp_dir"
+    cp "$1" "${1}_${2}_sw_temp_dir"
     mv "$2" "$1"
-    mv temp "$2"
+    mv "${1}_${2}_sw_temp_dir/$1" "./$2"
+    rmdir "${1}_${2}_sw_temp_dir"
 fi
