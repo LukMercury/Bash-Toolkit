40bffd0c932b3d81aadf3c37de0c9982318d4852
Mihai Luca <mihailuca406@gmail.com>
2019-08-01 19:54:22 +0300

    automatic startup commit

--- sw.sh
+++ sw.sh
@@ -4,8 +4,8 @@
 
 if [ -f "$1" ] && [ -f "$2" ]; then
     mkdir "${1}_${2}_sw_temp_dir"
-    cp "$1" "${1}_${2}_sw_temp_dir"
-    mv "$2" "$1"
-    mv "${1}_${2}_sw_temp_dir/$1" "./$2"
+    mv -n "$1" "${1}_${2}_sw_temp_dir"
+    mv -n "$2" "$1"
+    mv -n "${1}_${2}_sw_temp_dir/$1" "./$2"
     rmdir "${1}_${2}_sw_temp_dir"
 fi
