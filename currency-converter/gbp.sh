9a2413105f85aee1fb01a6e1c8fbcdd7a5a3a71f
Mihai Luca <mihailuca406@gmail.com>
2019-08-02 12:56:58 +0300

    code cleanup

--- currency-converter/gbp.sh
+++ currency-converter/gbp.sh
@@ -3,21 +3,21 @@
 # Convert USD to RON
 # Usage: usd [amount]
 
-EUR_RON=$(curl -s https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml | grep RON | cut -d= -f3 | tr -d "/>'")
-EUR_GBP=$(curl -s https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml | grep GBP | cut -d= -f3 | tr -d "/>'")
+EUR_RON="$(curl -s https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml | grep RON | cut -d= -f3 | tr -d "/>'")"
+EUR_GBP="$(curl -s https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml | grep GBP | cut -d= -f3 | tr -d "/>'")"
 
-GBP_RON=$(bc<<<"scale=4; $EUR_RON/$EUR_GBP")
+GBP_RON="$(bc<<<"scale=4; $EUR_RON/$EUR_GBP")"
 
-if [ -z $1 ]; then
+if [ -z "$1" ]; then
     AMOUNT=1
-elif [[ $1 =~ ^[0-9]+\.?[0-9]*$ ]]; then
-    AMOUNT=$1
+elif [[ "$1" =~ ^[0-9]+\.?[0-9]*$ ]]; then
+    AMOUNT="$1"
 else
     echo "Amount must be a number"
     exit 1
 fi
 
-RON=$(bc<<<"$AMOUNT * $GBP_RON")
+RON="$(bc<<<"$AMOUNT * $GBP_RON")"
 
 echo "$RON ron"
 
