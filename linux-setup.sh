395478d50994216d13e22494e560ebbf31185b48
Mihai Luca <mihailuca406@gmail.com>
2019-08-28 13:51:31 +0300

    automatic startup commit

--- linux-setup.sh
+++ linux-setup.sh
@@ -47,11 +47,11 @@ exec 2> install-log.txt # send error stream to log file
 # REPOSITORIES
 
 # REPOSITORIES/Sublime Text
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+wget -qO- https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 sudo bash -c 'echo "deb https://download.sublimetext.com/ apt/stable/" > /etc/apt/sources.list.d/sublime-text.list'
 
 # REPOSITORIES/Wine
-wget -nc https://dl.winehq.org/wine-builds/Release.key -O - | sudo apt-key add -
+wget -nc https://dl.winehq.org/wine-builds/Release.key -O- | sudo apt-key add -
 sudo apt-add-repository -y "deb https://dl.winehq.org/wine-builds/ubuntu/ $UBUNTU_CODENAME main"
 
 # REPOSITORIES/Lutris
@@ -61,7 +61,7 @@ sudo add-apt-repository -y ppa:lutris-team/lutris
 sudo add-apt-repository -y ppa:nilarimogard/webupd8
 
 # REPOSITORIES/Typora
-wget -qO - https://typora.io/linux/public-key.asc | sudo apt-key add -
+wget -qO- https://typora.io/linux/public-key.asc | sudo apt-key add -
 sudo add-apt-repository -y 'deb https://typora.io/linux ./'
 
 
