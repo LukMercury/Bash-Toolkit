0577938c66dff11445a82d87cd7b763f6d8e8f84
Mihai Luca <mihailuca406@gmail.com>
2019-08-28 13:41:42 +0300

    automatic startup commit

--- linux-setup.sh
+++ linux-setup.sh
@@ -51,7 +51,7 @@ wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add
 sudo bash -c 'echo "deb https://download.sublimetext.com/ apt/stable/" > /etc/apt/sources.list.d/sublime-text.list'
 
 # REPOSITORIES/Wine
-wget -nc https://dl.winehq.org/wine-builds/Release.key -O- | sudo apt-key add -
+wget -nc https://dl.winehq.org/wine-builds/Release.key -O - | sudo apt-key add -
 sudo apt-add-repository -y "deb https://dl.winehq.org/wine-builds/ubuntu/ $UBUNTU_CODENAME main"
 
 # REPOSITORIES/Lutris
