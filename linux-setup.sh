5fa4637b4ec2b8fe6e19ddaf81e5016171c28b52
Mihai Luca <mihailuca406@gmail.com>
2019-10-22 10:15:26 +0300

    automatic startup commit

--- linux-setup.sh
+++ linux-setup.sh
@@ -95,6 +95,7 @@ sudo apt install -y errno
 sudo apt install -y pstree
 sudo apt install -y finger
 sudo apt install -y xclip
+sudo apt install -y rwho
 sudo apt install -y openssh-server
 sudo apt install -y sshfs
 sudo apt install -y nnn
