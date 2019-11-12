f847171fab653b689c0becf6c3e392bcf2b4e8cd
Mihai Luca <mihailuca406@gmail.com>
2019-11-12 07:19:51 +0200

    automatic startup commit

--- sw.sh
+++ sw.sh
@@ -6,7 +6,8 @@ if [ -f "$1" ] && [ -f "$2" ]; then
     TEMP_DIR="_sw_temp_dir"
     mkdir "$TEMP_DIR"
     mv -n "$1" "$TEMP_DIR"
+    FILE1="$(basename $1)"
     mv -n "$2" "$1"
-    mv -n "$TEMP_DIR/$1" "./$2"
+    mv -n "$TEMP_DIR/${FILE1}" "$2"
     rmdir "$TEMP_DIR"
 fi
