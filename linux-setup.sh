eec937d3692580a76cce516a54017beb443f5575
Mihai Luca <mihailuca406@gmail.com>
2020-02-16 18:43:40 +0200

    improved linux-setup.sh

--- linux-setup.sh
+++ linux-setup.sh
@@ -210,7 +210,7 @@ sudo apt install -y audacious-plugins
 sudo apt install -y vlc
 sudo apt install -y finch
 sudo apt install -y alacarte
-sudo apt install -y caja-dropbox
+sudo apt install -y nautilus-dropbox
 sudo apt install -y qbittorrent
 sudo apt install -y dict
 sudo apt install -y dictd
