e0e01d8030f1ae2ddb246f319d1a01b8aba8685e
Mihai Luca <mihailuca406@gmail.com>
2020-05-26 01:22:26 +0300

    modified song.sh to display currently playing in terminal as well

--- cmus-local/song.sh
+++ cmus-local/song.sh
@@ -2,5 +2,6 @@
 
 # Display currently playing
 
+echo "~/$(cmus-remote -C status | head -2 | tail -1 | cut -d' ' -f2- | cut -d'/' -f4-)"
 notify-send "Currently playing" "~/$(cmus-remote -C status | head -2 | tail -1 | cut -d' ' -f2- | cut -d'/' -f4-)"
 
