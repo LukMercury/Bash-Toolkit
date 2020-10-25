ca99be4303fdc0def6feb63d6137c01331c11ca2
Mihai Luca <mihailuca406@gmail.com>
2020-10-25 21:33:31 +0200

    internet hotspot bridge

--- bridge.sh
+++ bridge.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sudo sysctl -w net.ipv4.ip_forward=1
 sudo ifconfig enp4s0 192.168.0.101 netmask 255.255.255.0 up
 sudo iptables -t nat -A POSTROUTING -o wlp2s0 -j MASQUERADE
 sudo iptables -I FORWARD -o wlp2s0 -s 192.168.0.0/16 -j ACCEPT
