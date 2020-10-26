0e6bed66f9f8f79f3183e5aca6b7ae206c250830
Mihai Luca <mihailuca406@gmail.com>
2020-10-26 10:25:06 +0200

    automatic startup commit

--- bridge.sh
+++ bridge.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+PC_IP=192.168.0.101
+WIRED_INTERFACE=enp4s0
+WIRELESS_INTERFACE=wlp2s0
+
 sudo sysctl -w net.ipv4.ip_forward=1
-sudo ifconfig enp4s0 192.168.0.101 netmask 255.255.255.0 up
-sudo iptables -t nat -A POSTROUTING -o wlp2s0 -j MASQUERADE
-sudo iptables -I FORWARD -o wlp2s0 -s 192.168.0.0/16 -j ACCEPT
+sudo ifconfig $WIRED_INTERFACE $PC_IP netmask 255.255.255.0 up
+sudo iptables -t nat -A POSTROUTING -o $WIRELESS_INTERFACE -j MASQUERADE
+sudo iptables -I FORWARD -o $WIRELESS_INTERFACE -s 192.168.0.0/16 -j ACCEPT
 sudo iptables -I INPUT -s 192.168.0.0/16 -j ACCEPT
