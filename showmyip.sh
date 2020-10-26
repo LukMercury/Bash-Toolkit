4b86936bce9eb68afa8694fb4e30c677ad823249
Mihai Luca <mihailuca406@gmail.com>
2020-10-26 13:21:49 +0200

    check for ip

---
+++ showmyip.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+curl ipinfo.io/ip
+
+# curl ifconfig.me
+# curl -4/-6 icanhazip.com
+# curl api.ipify.org
+# curl checkip.dyndns.org
+# dig +short myip.opendns.com @resolver1.opendns.com
+# host myip.opendns.com resolver1.opendns.com
+# curl ident.me
+# curl bot.whatismyipaddress.com
+# curl ipecho.net/plain
