a7cd6da3801fd82a7a7c8293775bad08b457f480
Mihai Luca <mihailuca406@gmail.com>
2022-09-30 10:48:24 +0300

    automatic startup commit

--- output-Schiit.sh
+++ output-Schiit.sh
@@ -2,8 +2,8 @@
 
 COUNT=$(pactl list short sink-inputs | wc -l)
 
-for i in {1..$COUNT}; do
+for i in $(seq $COUNT); do
 
-pactl move-sink-input $(pactl list short sink-inputs | head -n $COUNT | tail -n $i | tr '\t' ' ' | cut -d' ' -f1) alsa_output.usb-Schiit_Audio_Schiit_Modi_3E-00.iec958-stereo
+pactl move-sink-input $(pactl list short sink-inputs | head -n $i | tail -1 | tr '\t' ' ' | cut -d' ' -f1) alsa_output.usb-Schiit_Audio_Schiit_Modi_3E-00.2.iec958-stereo
 
 done
