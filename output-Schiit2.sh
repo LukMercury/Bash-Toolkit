c5820af87fa8776655d1d66482d46f83ebed6c6c
Mihai Luca <mihailuca406@gmail.com>
2023-05-05 17:11:17 +0300

    new sound outputs

--- output-Schiit2.sh
+++ output-Schiit2.sh
@@ -6,6 +6,6 @@ COUNT=$(pactl list short sink-inputs | wc -l)
 
 for i in $(seq $COUNT); do
 
-pactl move-sink-input $(pactl list short sink-inputs | head -n $i | tail -1 | tr '\t' ' ' | cut -d' ' -f1) alsa_output.usb-GuangZhou_FiiO_Electronics_Co._Ltd_FiiO_K5_Pro-00.iec958-stereo
+pactl move-sink-input $(pactl list short sink-inputs | head -n $i | tail -1 | tr '\t' ' ' | cut -d' ' -f1) alsa_output.usb-Schiit_Audio_Schiit_Modi_-00.iec958-stereo
 
 done
