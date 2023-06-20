65daa8f7de68e553bd6e438dffa05fde5fb5bb63
Mihai Luca <mihailuca406@gmail.com>
2023-06-20 11:42:17 +0300

    automatic startup commit

---
+++ graphics-fix.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo apt install mesa-vulkan-drivers=22.0.1-1ubuntu2
+sudo apt install mesa-vulkan-drivers:i386=22.0.1-1ubuntu2
+sudo apt install mesa-common-dev=22.0.1-1ubuntu2
+sudo apt install mesa-va-drivers=22.0.1-1ubuntu2
+sudo apt install mesa-vdpau-drivers=22.0.1-1ubuntu2
