ed648474bfdc17904a37b46c1e6d3bd7b246c60d
Mihai Luca <mihailuca406@gmail.com>
2023-10-27 17:25:23 +0300

    improved display of currently-playing on local and remote

--- cmus-local/song.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Display currently playing
-
-echo "~/$(cmus-remote -C status | head -2 | tail -1 | cut -d' ' -f2- | cut -d'/' -f4-)"
-notify-send "Currently playing" "~/$(cmus-remote -C status | head -2 | tail -1 | cut -d' ' -f2- | cut -d'/' -f4-)"
-
